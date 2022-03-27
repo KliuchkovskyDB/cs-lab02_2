479d0efaa3c75b9f32dbcb97122010d7542a6fc4
Alice (KluckovskiyDB) <lakvain@gmail.com>
2022-03-28 02:14:42 +0300

    code: вывод максимума

--- main.cpp
+++ main.cpp
@@ -5,10 +5,17 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B: ";
-    int a, b;
+    int a, b, m;
     cin >> a >> b;
+    if (a > b){
+        m = a;
+    }
+    else{
+        m = b;
+    }
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
-         << "A / B = " << a / b << '\n';
+         << "A / B = " << a / b << '\n'
+         << "max = " << m << '\n';
 }
